05e4451821b8ca37eb17c259575f354b6b0a1920
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-11-23 07:31:19 -0500

    Files generated by PkgTemplates

---
+++ src/SciPyDiffEq.jl
@@ -0,0 +1,5 @@
+module SciPyDiffEq
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SciPyDiffEq
+using Test
+
+@testset "SciPyDiffEq.jl" begin
+    # Write your own tests here.
+end
