897e961bae898a8e61faacc3337fea76152892bc
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-07-12 18:02:11 -0400

    Update SciPyDiffEq QA to SciMLTesting 2.1
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- test/qa/qa.jl
+++ test/qa/qa.jl
@@ -3,10 +3,9 @@ using SciMLTesting, SciPyDiffEq, JET, Test
 run_qa(
     SciPyDiffEq;
     explicit_imports = true,
-    jet_kwargs = (; target_defined_modules = true),
+    jet_kwargs = (; target_modules = (SciPyDiffEq,)),
     ei_kwargs = (;
-        # SciMLBase-owned interface names extended/used here that are still not
-        # declared public in SciMLBase 3.27.0 (verified Base.ispublic == false).
+        # SciMLBase-owned interface names extended/used here that are not public API.
         all_qualified_accesses_are_public = (;
             ignore = (
                 :AbstractDiffEqInterpolation, :LinearInterpolation, :__solve, :interp_summary,
