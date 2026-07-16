ce038fc6a41db4a47cb449188017b57921bbdd0e
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-07-16 17:33:55 -0400

    Enable rendered API docs QA in SciPyDiffEq
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- test/qa/qa.jl
+++ test/qa/qa.jl
@@ -2,6 +2,10 @@ using SciMLTesting, SciPyDiffEq, JET, Test
 
 run_qa(
     SciPyDiffEq;
+    api_docs_kwargs = (;
+        rendered = true,
+        rendered_ignore = Tuple(names(SciPyDiffEq.DiffEqBase)),
+    ),
     explicit_imports = true,
     jet_kwargs = (; target_modules = (SciPyDiffEq,)),
     ei_kwargs = (;
